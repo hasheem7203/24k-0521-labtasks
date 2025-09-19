ead6873b77f1a33d6c69da55685108f1c1e69c60
Muhammad Hasheem Shuja <muhammadhasheemshuja@gmail.com>
2025-09-19 15:43:39 +0500

    Create task1.cpp

---
+++ lab-04/task1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[]={64,34,25,12,22,11,90};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int comparisons=0;
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            comparisons++;
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+
+    cout<<"Sorted Element List: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"Total comparisons = "<<comparisons<<endl;
+
+    return 0;
+}
