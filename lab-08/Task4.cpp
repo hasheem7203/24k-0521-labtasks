3fb3b077820322780571601b4002652c2882a854
Muhammad Hasheem Shuja <muhammadhasheemshuja@gmail.com>
2025-11-09 20:11:53 +0500

    Implement subtree sum finder in binary tree

---
+++ lab-08/Task4.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data) : data(data), left(NULL), right(NULL) {}
+};
+
+Node* createNode(int data){
+	return new Node(data);
+}
+
+int subtreesum(Node* node ){
+    if(node ==NULL) return 0;
+
+    return node->data+subtreesum(node->left)+subtreesum(node->right);
+}
+void display(Node *node, int level)
+{
+    if (node == NULL)   return;
+    display(node->right, level + 1);
+    for (int i = 0; i < level; i++){
+        cout << "\t";
+    }
+    cout<<" "<<node->data << endl;
+    display(node->left, level + 1);
+}
+
+bool findSubtreeOfSum(Node* node,int targetSum){
+    if(node==NULL) return false;
+    int sum =subtreesum(node);
+    if(sum==targetSum){
+        display(node,0);
+        return true;
+    }
+    return findSubtreeOfSum(node->left,targetSum)||findSubtreeOfSum(node->right,targetSum);
+}
+
+
+
+int main(){
+	
+    Node* n1=new Node(5);
+	n1->right=createNode(10);
+	n1->left=createNode(20);
+	n1->right->right = createNode(30);
+	n1->right->left = createNode(40);
+	n1->left->right = createNode(50);
+	n1->left->left = createNode(60);
+	n1->left->left->left = createNode(70);
+
+int sum=80;
+
+if(findSubtreeOfSum(n1,sum)){
+    cout <<"Subtree with sum "<<sum<<" found ";
+}else {
+    cout <<"Subtree with sum "<<sum<<" not found ";
+}
+cout <<"\n\n\n";
+display(n1,0);
+
+}
