38eaace7fdd9f648b15deb20d9e595db5613f0f8
Muhammad Hasheem Shuja <muhammadhasheemshuja@gmail.com>
2025-11-14 14:50:39 +0500

    Add AVL tree implementation in Task3.cpp
    Implement AVL tree with insertion and in-order traversal

---
+++ lab-09/Task3.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int key;
+    Node *left, *right;
+    int height;
+
+    Node(int k) {
+        key = k;
+        left = right = NULL;
+        height = 1;
+    }
+};
+
+int getHeight(Node *n) {
+    return (n == NULL) ? 0 : n->height;
+}
+
+int getMax(int a, int b) {
+    return (a > b) ? a : b;
+}
+
+int getBalance(Node *n) {
+    if (n == NULL) return 0;
+    return getHeight(n->left) - getHeight(n->right);
+}
+
+Node* leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    x->height = 1 + getMax(getHeight(x->left), getHeight(x->right));
+    y->height = 1 + getMax(getHeight(y->left), getHeight(y->right));
+
+    return y;
+}
+
+Node* rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+
+    x->right = y;
+    y->left = T2;
+
+    y->height = 1 + getMax(getHeight(y->left), getHeight(y->right));
+    x->height = 1 + getMax(getHeight(x->left), getHeight(x->right));
+
+    return x;
+}
+
+Node* insert(Node* node, int key) {
+    if (node == NULL)
+        return new Node(key);
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else
+        return node;
+
+    node->height = 1 + getMax(getHeight(node->left), getHeight(node->right));
+    int balance = getBalance(node);
+
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    if (balance > 1 && key > node->left->key) {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    if (balance < -1 && key < node->right->key) {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    return node;
+}
+
+void inOrder(Node* root) {
+    if (root != NULL) {
+        inOrder(root->left);
+        cout << root->key << " ";
+        inOrder(root->right);
+    }
+}
+
+int main() {
+    Node *root = NULL;
+
+    int arr[] = {10,5,15,3,7};
+    for (int x : arr)
+        root = insert(root, x);
+
+    root = insert(root, 12);
+
+      cout << endl;
+
+    cout << "Height of AVL tree: " << getHeight(root) << endl;
+    cout << "Preorder after inserting 15: ";
+    inOrder(root);
+  
+
+    return 0;
+}
